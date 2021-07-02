25e3109215090bca827764903cb9ae722d6538ee
Anna Opareva <v-aopareva@microsoft.com>
2021-07-02 10:19:31 +0300

    fix request environmentName

--- src/Agent.Listener/Configuration/ConfigurationManager.cs
+++ src/Agent.Listener/Configuration/ConfigurationManager.cs
@@ -271,7 +271,6 @@ namespace Microsoft.VisualStudio.Services.Agent.Listener.Configuration
             while (true)
             {
                 agentSettings.AgentName = command.GetAgentName();
-                agentSettings.EnvironmentName = command.GetEnvironmentName();
 
                 // Get the system capabilities.
                 // TODO: Hook up to ctrl+c cancellation token.
